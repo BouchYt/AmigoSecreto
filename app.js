7b393929db240abcc0f30cb8e4ad7726d5a4b159
Jose <jair.guerreroaq@gmail.com>
2025-03-17 23:11:50 -0600

    Funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
 let amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+}
